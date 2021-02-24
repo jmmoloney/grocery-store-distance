3d927e786d75ed9f50576b8be8fc6ca470bb1a9f
jmmoloney <jmmoloney18@gmail.com>
2021-02-23 21:01:39 -0500

    changing from requests rest call to googlemaps library

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # grocery-store-distance
+
+Utilizes the Google Places API to determine the closest/most prominent stores
+per toronto neightbourhood.
+
+API documentation  
+https://developers.google.com/places/web-service/details  
+
+Next Steps/To dos:
+- instead of blanket distance from average lat/long, customize sliding distance calculations across areas in neighbourhood
+- UI embeded to switch queries -- whether interested in closest, applying custom
+prominence filter, just prominence, etc. (median, average distance?)
--- main.py
+++ main.py
@@ -1,54 +1,41 @@
-import json
-import requests
-import urllib
+import googlemaps
 from config import Config
+from testing import load_neighbourhood_data
 
 def main():
-	return 4
-
+	return 7
 
 if __name__ == '__main__':
 	config = Config().config
 	filepath = config["NEIGHBOURHOOD DATA"]["FILEPATH"]
-	from testing import load_neighbourhood_data
-	# load acceptable grocery stores
-	# grocery_store_names.txt
-	# parse geojson instead?
+	key = config["MAIN"]["API_KEY"]
 
 	df = load_neighbourhood_data(filepath)
-	# print(df["AREA_NAME"].unique().tolist())
-	# cycle through for each latitude and longitude
-	# lat = df.query("AREA_NAME == 'Kensington-Chinatown (78)'").LATITUDE.values
-	# long = df.query("AREA_NAME == 'Kensington-Chinatown (78)'").LONGITUDE.values
+
 	area = 'Waterfront Communities-The Island (77)'
-	lat = df.query("AREA_NAME == '{}'".format(area)).LATITUDE.values
-	long = df.query("AREA_NAME == '{}'".format(area)).LONGITUDE.values
+	lat_long = df.query("AREA_NAME == '{}'".format(area))
+	lat, long = lat_long.LATITUDE.values, lat_long.LONGITUDE.values
+	location= (lat, long)
 
-
-	output = "json"
-	key = config["MAIN"]["API_KEY"]
-	inputtype="textquery"
-	keyword = "Grocery Store".replace(' ', '%20')
+	# inputtype="textquery"
+	keyword = "Grocery Store"
 
 	radius = 2000 # in metres
-	location=f"{lat[0]},{long[0]}"
-	rankby='distance'
-	# query = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/{output}?key={key}&inputtype={inputtype}&input={text}&locationbias={locationbias}"
-	query = f"https://maps.googleapis.com/maps/api/place/nearbysearch/{output}?key={key}&location={location}&keyword={keyword}&rankby={rankby}"
-	# resp = requests.get(query)
-	# if resp.status_code == requests.codes.ok:
-		# with open("waterfront_response.json", "w") as f:
-			# f.write(resp.text)
-	# rankby distance, could also do prominence
-	with open("waterfront_response.json", 'r') as f:
-		file = json.load(f)
-	results=file["results"]
-	sample = results[0]
-	# print(sample)
-	# print(sample.keys())
-	location_name = sample['name']
-	# distance_from_nb = sample[]
-	location_lat = sample["geometry"]["location"]["lat"]
-	location_long = sample["geometry"]["location"]["lng"]
-	address = sample["vicinity"]
-	# TODO: use the requests library to format the request
+	rank_by='prominence' #distance # cannot have radius when rankby is prominence
+
+	gmaps = googlemaps.Client(key=key)
+	result = gmaps.places_nearby(location=location, radius=radius, keyword=keyword,
+								 rank_by=rank_by)
+	print(result)
+
+	# location=None,
+    # radius=None,
+    # keyword=None,
+    # language=None,
+    # min_price=None,
+    # max_price=None,
+    # name=None,
+    # open_now=False,
+    # rank_by=None,
+    # type=None,
+    # page_token=None,
